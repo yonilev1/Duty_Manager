1951610646b032a5e38d26479b28572152d8df57
Yoni <yonilev2021@gmail.com>
2026-05-20 10:09:14 +0300

    added func to check if day valid (in utils) and fun to get soldiers duties ( in duty_manager)

--- utils.py
+++ utils.py
@@ -1,4 +1,19 @@
+import data
+
 def is_valid_name(name: str) -> bool:
     return 0 < len(name) <= 20 
     
-print(is_valid_name("ge"))
+
+def is_valid_ma(ma:int):
+    return ma < 1000000000
+
+
+def find_soldier_by_id(ma):
+    for soldier in data.soldier_list:
+        if soldier["ma"] == ma:
+            return soldier
+    return None
+
+
+def is_valid_status(status: str) -> bool:
+    return status.lower() in ["sunday", "monday", "tuesday", "wednesday", "thursday"]
