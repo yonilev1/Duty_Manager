9b187b506b2d7dd8870f4ffafbdc8d780faeb0d5
Yoni <yonilev2021@gmail.com>
2026-05-20 10:24:47 +0300

    added last function in soldier_manager.py - get_all_soldiers

--- soldier_manager.py
+++ soldier_manager.py
@@ -21,7 +21,9 @@ def remove_soldier(ma: int) -> None:
         data.soldier_list.remove(index)
 
 
-
+def get_all_soldiers() -> list:
+    for soldier in data.soldier_list:
+        print(f"=== ma: {soldier["ma"]}, name: {soldier["name"]} ===")
 
        
 
